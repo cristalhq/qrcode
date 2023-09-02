7464601ddd2e65315f3eda2bc03df097ffd49232
Oleg Kovalov <oleg@hey.com>
2023-09-02 13:24:20 +0200

    Reorder GF255 declarations (#2)

--- internal/gf256/gf256.go
+++ internal/gf256/gf256.go
@@ -2,169 +2,6 @@ package gf256
 
 import "strconv"
 
-// Field represents an instance of GF(256) defined by a specific polynomial.
-type Field struct {
-	log [256]byte // log[0] is unused
-	exp [510]byte
-}
-
-// NewField returns a new field corresponding to the polynomial poly
-// and generator alpha. The Reed-Solomon encoding in QR codes uses
-// polynomial 0x11d with generator 2.
-//
-// The choice of generator alpha only affects the Exp and Log operations.
-func NewField(poly, alpha int) *Field {
-	if poly < 0x100 || poly >= 0x200 || reducible(poly) {
-		panic("gf256: invalid polynomial: " + strconv.Itoa(poly))
-	}
-
-	var f Field
-	x := 1
-	for i := 0; i < 255; i++ {
-		if x == 1 && i != 0 {
-			panic("gf256: invalid generator " + strconv.Itoa(alpha) +
-				" for polynomial " + strconv.Itoa(poly))
-		}
-		f.exp[i] = byte(x)
-		f.exp[i+255] = byte(x)
-		f.log[x] = byte(i)
-		x = mul(x, alpha, poly)
-	}
-	f.log[0] = 255
-	for i := 0; i < 255; i++ {
-		if f.log[f.exp[i]] != byte(i) {
-			panic("gf256: bad log")
-		}
-		if f.log[f.exp[i+255]] != byte(i) {
-			panic("gf256: bad log")
-		}
-	}
-	for i := 1; i < 256; i++ {
-		if f.exp[f.log[i]] != byte(i) {
-			panic("gf256: bad log")
-		}
-	}
-	return &f
-}
-
-// reducible reports whether p is reducible.
-func reducible(p int) bool {
-	// Multiplying n-bit * n-bit produces (2n-1)-bit,
-	// so if p is reducible, one of its factors must be
-	// of np/2+1 bits or fewer.
-	np := nbit(p)
-	for q := 2; q < 1<<(np/2+1); q++ {
-		if polyDiv(p, q) == 0 {
-			return true
-		}
-	}
-	return false
-}
-
-// polyDiv divides the polynomial p by q and returns the remainder.
-func polyDiv(p, q int) int {
-	np := nbit(p)
-	nq := nbit(q)
-	for ; np >= nq; np-- {
-		if p&(1<<(np-1)) != 0 {
-			p ^= q << (np - nq)
-		}
-	}
-	return p
-}
-
-// nbit returns the number of significant in p.
-func nbit(p int) uint {
-	n := uint(0)
-	for ; p > 0; p >>= 1 {
-		n++
-	}
-	return n
-}
-
-// mul returns the product x*y mod poly, a GF(256) multiplication.
-func mul(x, y, poly int) int {
-	z := 0
-	for x > 0 {
-		if x&1 != 0 {
-			z ^= y
-		}
-		x >>= 1
-		y <<= 1
-		if y&0x100 != 0 {
-			y ^= poly
-		}
-	}
-	return z
-}
-
-// Add returns the sum of x and y in the field.
-func (f *Field) Add(x, y byte) byte {
-	return x ^ y
-}
-
-// Exp returns the base-alpha exponential of e in the field.
-// If e < 0, Exp returns 0.
-func (f *Field) Exp(e int) byte {
-	if e < 0 {
-		return 0
-	}
-	return f.exp[e%255]
-}
-
-// Log returns the base-alpha logarithm of x in the field.
-// If x == 0, Log returns -1.
-func (f *Field) Log(x byte) int {
-	if x == 0 {
-		return -1
-	}
-	return int(f.log[x])
-}
-
-// Inv returns the multiplicative inverse of x in the field.
-// If x == 0, Inv returns 0.
-func (f *Field) Inv(x byte) byte {
-	if x == 0 {
-		return 0
-	}
-	return f.exp[255-f.log[x]]
-}
-
-// Mul returns the product of x and y in the field.
-func (f *Field) Mul(x, y byte) byte {
-	if x == 0 || y == 0 {
-		return 0
-	}
-	return f.exp[int(f.log[x])+int(f.log[y])]
-}
-
-func (f *Field) gen(e int) (gen, lgen []byte) {
-	// p = 1
-	p := make([]byte, e+1)
-	p[e] = 1
-
-	for i := 0; i < e; i++ {
-		// p *= (x + Exp(i))
-		// p[j] = p[j]*Exp(i) + p[j+1].
-		c := f.Exp(i)
-		for j := 0; j < e; j++ {
-			p[j] = f.Mul(p[j], c) ^ p[j+1]
-		}
-		p[e] = f.Mul(p[e], c)
-	}
-
-	// lp = log p.
-	lp := make([]byte, e+1)
-	for i, c := range p {
-		if c == 0 {
-			lp[i] = 255
-		} else {
-			lp[i] = byte(f.Log(c))
-		}
-	}
-	return p, lp
-}
-
 // An RSEncoder implements Reed-Solomon encoding
 // over a given field using a given number of error correction bytes.
 type RSEncoder struct {
@@ -236,4 +73,162 @@ func (rs *RSEncoder) ECC(data, check []byte) {
 	}
 	copy(check, p[len(data):])
 	rs.p = p
+}
+
+// Field represents an instance of GF(256) defined by a specific polynomial.
+type Field struct {
+	log [256]byte // log[0] is unused
+	exp [510]byte
+}
+
+// NewField returns a new field corresponding to the polynomial poly
+// and generator alpha. The Reed-Solomon encoding in QR codes uses
+// polynomial 0x11d with generator 2.
+//
+// The choice of generator alpha only affects the Exp and Log operations.
+func NewField(poly, alpha int) *Field {
+	if poly < 0x100 || poly >= 0x200 || reducible(poly) {
+		panic("gf256: invalid polynomial: " + strconv.Itoa(poly))
+	}
+
+	var f Field
+	x := 1
+	for i := 0; i < 255; i++ {
+		if x == 1 && i != 0 {
+			panic("gf256: invalid generator " + strconv.Itoa(alpha) +
+				" for polynomial " + strconv.Itoa(poly))
+		}
+		f.exp[i] = byte(x)
+		f.exp[i+255] = byte(x)
+		f.log[x] = byte(i)
+		x = mul(x, alpha, poly)
+	}
+	f.log[0] = 255
+	for i := 0; i < 255; i++ {
+		if f.log[f.exp[i]] != byte(i) {
+			panic("gf256: bad log")
+		}
+		if f.log[f.exp[i+255]] != byte(i) {
+			panic("gf256: bad log")
+		}
+	}
+	for i := 1; i < 256; i++ {
+		if f.exp[f.log[i]] != byte(i) {
+			panic("gf256: bad log")
+		}
+	}
+	return &f
+}
+
+// Exp returns the base-alpha exponential of e in the field.
+// If e < 0, Exp returns 0.
+func (f *Field) Exp(e int) byte {
+	if e < 0 {
+		return 0
+	}
+	return f.exp[e%255]
+}
+
+// Log returns the base-alpha logarithm of x in the field.
+// If x == 0, Log returns -1.
+func (f *Field) Log(x byte) int {
+	if x == 0 {
+		return -1
+	}
+	return int(f.log[x])
+}
+
+// Inv returns the multiplicative inverse of x in the field.
+// If x == 0, Inv returns 0.
+func (f *Field) Inv(x byte) byte {
+	if x == 0 {
+		return 0
+	}
+	return f.exp[255-f.log[x]]
+}
+
+// Mul returns the product of x and y in the field.
+func (f *Field) Mul(x, y byte) byte {
+	if x == 0 || y == 0 {
+		return 0
+	}
+	return f.exp[int(f.log[x])+int(f.log[y])]
+}
+
+func (f *Field) gen(e int) (gen, lgen []byte) {
+	// p = 1
+	p := make([]byte, e+1)
+	p[e] = 1
+
+	for i := 0; i < e; i++ {
+		// p *= (x + Exp(i))
+		// p[j] = p[j]*Exp(i) + p[j+1].
+		c := f.Exp(i)
+		for j := 0; j < e; j++ {
+			p[j] = f.Mul(p[j], c) ^ p[j+1]
+		}
+		p[e] = f.Mul(p[e], c)
+	}
+
+	// lp = log p.
+	lp := make([]byte, e+1)
+	for i, c := range p {
+		if c == 0 {
+			lp[i] = 255
+		} else {
+			lp[i] = byte(f.Log(c))
+		}
+	}
+	return p, lp
+}
+
+// reducible reports whether p is reducible.
+func reducible(p int) bool {
+	// Multiplying n-bit * n-bit produces (2n-1)-bit,
+	// so if p is reducible, one of its factors must be
+	// of np/2+1 bits or fewer.
+	np := nbit(p)
+	for q := 2; q < 1<<(np/2+1); q++ {
+		if polyDiv(p, q) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// polyDiv divides the polynomial p by q and returns the remainder.
+func polyDiv(p, q int) int {
+	np := nbit(p)
+	nq := nbit(q)
+	for ; np >= nq; np-- {
+		if p&(1<<(np-1)) != 0 {
+			p ^= q << (np - nq)
+		}
+	}
+	return p
+}
+
+// nbit returns the number of significant in p.
+func nbit(p int) uint {
+	n := uint(0)
+	for ; p > 0; p >>= 1 {
+		n++
+	}
+	return n
+}
+
+// mul returns the product x*y mod poly, a GF(256) multiplication.
+func mul(x, y, poly int) int {
+	z := 0
+	for x > 0 {
+		if x&1 != 0 {
+			z ^= y
+		}
+		x >>= 1
+		y <<= 1
+		if y&0x100 != 0 {
+			y ^= poly
+		}
+	}
+	return z
 }
